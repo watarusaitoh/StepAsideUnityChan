fa34331f3f4148ffbb78c267d6693d3e86bbe989
watarusaitoh <fadakk7@gmail.com>
2021-01-07 15:38:09 +0900

    Initial commit

---
+++ Assets/DestroyLineController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyLineController : MonoBehaviour
+{
+    //unitychanのオブジェクト
+    private GameObject unitychan;
+    //Unitychanとの距離
+    private float deifference;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Unitychanのオブジェクトを取得
+        unitychan = GameObject.Find("unitychan");
+        //DestroyLineとUnitychanの距離を設定
+        this.deifference = this.unitychan.transform.position.z - this.transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Unitychanに一定の距離を保って移動
+        this.transform.position = new Vector3(0f, this.transform.position.y, this.unitychan.transform.position.z - this.deifference);
+    }
+
+
+    //DestroyLineオブジェクトに当たったアイテムを破棄する
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag =="CoinTag")
+        {
+            Destroy(other.gameObject);
+        }
+        if (other.gameObject.tag =="TrafficConeTag")
+        {
+            Destroy(other.gameObject);
+        }
+        if (other.gameObject.tag == "CarTag")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+    
+    
+}
---
+++ Assets/GenerateAreaController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateAreaController : MonoBehaviour
+{
+    public GameObject CoinPrefab;
+    public GameObject ConePrefab;
+    public GameObject CarPrefab;
+
+    private GameObject unitychan;
+    private float difference;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        this.unitychan = GameObject.Find("unitychan");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = new Vector3(0, this.transform.position.y, this.unitychan.transform.position.z);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "CarTag")
+        {
+           
+        }
+        if (other.gameObject.tag == "TrafficConeTag")
+        {
+            
+        }
+    }
+}
